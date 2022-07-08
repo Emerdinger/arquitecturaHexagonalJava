14d87b49f2c35b4179aa555ad0dba27df516e23d
Emerdinger <servicesemer@gmail.com>
2022-07-08 11:17:34 -0500

    test

--- src/test/java/com/emer/arquitectura/ArquitecturaApplicationTests.java
+++ src/test/java/com/emer/arquitectura/ArquitecturaApplicationTests.java
@@ -1,13 +1,49 @@
 package com.emer.arquitectura;
 
+import com.emer.arquitectura.aplicacion.manejador.ManejadorObtenerProducto;
+import com.emer.arquitectura.dominio.modelo.Producto;
+import com.emer.arquitectura.infraestructura.endpoint.EndpointObtenerProducto;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 
-@SpringBootTest
+import java.time.LocalDate;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(controllers = EndpointObtenerProducto.class)
+@Import(ManejadorObtenerProducto.class)
 class ArquitecturaApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@MockBean
+	ManejadorObtenerProducto manejadorObtenerProducto;
+
 	@Test
-	void contextLoads() {
+	public void obtenerTodos() {
+		Flux<Producto> prodcutos = Flux.just(new Producto("1", "Laptop","Test", "Asus", "Gamer", 1, 250.0, LocalDate.now(), "laptop.img"));
+		Mockito.when(manejadorObtenerProducto.ejecutarObtenerTodos()).thenReturn(prodcutos);
+		webTestClient.get()
+				.uri("/productos/all")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$[0].id").isEqualTo("1");
 	}
+
 
 }
